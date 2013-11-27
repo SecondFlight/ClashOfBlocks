d3105603c22440b67fae32263f1b33e7b00c166c
Aperx <aperxmc@gmail.com>
2013-11-27 21:56:15 +1030

    Added Help GUI
    Added the start of a Help GUI

---
+++ src/com/github/cob/Commands/Testy.java
@@ -0,0 +1,25 @@
+package com.github.cob.Commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.github.cob.Inventories.HelpInventory;
+
+public class Testy implements CommandExecutor{
+
+	public boolean onCommand(CommandSender sender, Command cmd, String CommandLabel, String[] args){
+        Player p = (Player) sender;
+		
+        if(cmd.getName().equalsIgnoreCase("help")){
+        	p.openInventory(HelpInventory.HelpMenu);
+        }
+        
+        return false;
+        
+	}
+	
+	
+
+}
---
+++ src/com/github/cob/Listeners/HelpInvListeners.java
@@ -0,0 +1,66 @@
+package com.github.cob.Listeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+public class HelpInvListeners implements Listener{
+	
+	@EventHandler
+	public void ExitInventory(InventoryClickEvent event) {
+	Player player = (Player) event.getWhoClicked(); 
+	ItemStack clicked = event.getCurrentItem(); 
+	Inventory inventory = event.getInventory(); 
+	
+	if (inventory.getName().equals(ChatColor.GOLD+ ChatColor.BOLD.toString()+"COB: " +ChatColor.RESET + ChatColor.DARK_GREEN +"Help Menu")) {
+		if (clicked.getType() == Material.LAVA_BUCKET) { 
+			if(clicked.getItemMeta().getDisplayName().equals(ChatColor.DARK_RED+"Exit")){
+			event.setCancelled(true); 
+			player.closeInventory(); 
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void AdminHelp(InventoryClickEvent event) {
+	Player player = (Player) event.getWhoClicked(); 
+	ItemStack clicked = event.getCurrentItem(); 
+	Inventory inventory = event.getInventory(); 
+	
+	if (inventory.getName().equals(ChatColor.GOLD+ ChatColor.BOLD.toString()+"COB: " +ChatColor.RESET + ChatColor.DARK_GREEN +"Help Menu")) {
+		if (clicked.getType() == Material.FIREBALL) { 
+			if(clicked.getItemMeta().getDisplayName().equals(ChatColor.DARK_RED+"Admin Help")){
+				if(player.hasPermission("cob.adminhelp")){
+			event.setCancelled(true); 
+			//Open admin help menu
+					}
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void PlayerHelp(InventoryClickEvent event) {
+	Player player = (Player) event.getWhoClicked(); 
+	ItemStack clicked = event.getCurrentItem(); 
+	Inventory inventory = event.getInventory(); 
+	
+	if (inventory.getName().equals(ChatColor.GOLD+ ChatColor.BOLD.toString()+"COB: " +ChatColor.RESET + ChatColor.DARK_GREEN +"Help Menu")) {
+		if (clicked.getType() == Material.MAGMA_CREAM) { 
+			if(clicked.getItemMeta().getDisplayName().equals(ChatColor.DARK_GREEN+"Player Help")){
+				if(player.hasPermission("cob.playerhelp") || player.hasPermission("cob.adminhelp")){
+			event.setCancelled(true); 
+			//Open player help menu
+					}
+				}
+			}
+		}
+	}
+
+}
