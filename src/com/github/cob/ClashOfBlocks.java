7f82697edafb07eab918a8e12e688cda64217222
Aperx <aperxmc@gmail.com>
2013-11-27 19:17:52 +1030

    Main class added

---
+++ src/com/github/cob/ClashOfBlocks.java
@@ -0,0 +1,15 @@
+package com.github.cob;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ClashOfBlocks extends JavaPlugin {
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+
+}
