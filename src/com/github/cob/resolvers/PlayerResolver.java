f0f85fc5732892ffed7a637aaf612fb80c984b3b
Erik <erikrosem@gmail.com>
2014-01-25 20:01:29 +0100

    Updated Player Resolver

--- src/com/github/cob/resolvers/PlayerResolver.java
+++ src/com/github/cob/resolvers/PlayerResolver.java
@@ -13,19 +13,19 @@ public class PlayerResolver {
     //Can Afford Methods, return if the player has enough of the specified currency to afford something.
 
     public boolean canAffordWithGold(Player player, int gold)  {
-        return ClashOfBlocks.getInstance().getGold().getGold(player.getName()) > gold;
+        return ClashOfBlocks.getInstance().getGold().getGold(player.getName()) >= gold;
     }
 
     public boolean canAffordWithGems(Player player, int gems)  {
-        return ClashOfBlocks.getInstance().getGems().getGems(player.getName()) > gems;
+        return ClashOfBlocks.getInstance().getGems().getGems(player.getName()) >= gems;
     }
 
     public boolean canAffordWithElixir(Player player, int elixir)  {
-        return ClashOfBlocks.getInstance().getElixir().getElixir(player.getName()) > elixir;
+        return ClashOfBlocks.getInstance().getElixir().getElixir(player.getName()) >= elixir;
     }
 
     public boolean canAffordWithDarkElixir(Player player, int darkElixir)  {
-        return ClashOfBlocks.getInstance().getDarkElixir().getDarkElixir(player.getName()) > darkElixir;
+        return ClashOfBlocks.getInstance().getDarkElixir().getDarkElixir(player.getName()) >= darkElixir;
     }
 
     //*************************//
